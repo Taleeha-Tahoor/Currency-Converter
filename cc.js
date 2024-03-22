57684d1ffe935ee9924bc67a05e7e25f176e303e
Taleeha <taleeha789@gmail.com>
2024-03-22 11:21:14 +0500

    Project Added

---
+++ cc.js
@@ -0,0 +1,60 @@
+const BASE_URL = 'https://open.er-api.com/v6/latest';
+
+const dropdowns = document.querySelectorAll('.dropdown select');
+const btn = document.querySelector('form button');
+const fromCurr = document.querySelector('.from select');
+const toCurr = document.querySelector('.to select');
+const msg = document.querySelector('.msg');
+
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement('option');
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+
+        if (select.name === 'from' && currCode === 'USD') {
+            newOption.selected = 'selected';
+        } else if (select.name === 'to' && currCode === 'PKR') { // Corrected 'PK' from 'PKR'
+            newOption.selected = 'selected';
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener('change', (evt) => {
+        updateFlag(evt.target);
+    })
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+
+    if (amtVal == "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}`;
+    try {
+        let response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        let data = await response.json();
+        let rate = data.rates[toCurr.value.toUpperCase()];
+        let finalAmount = amtVal * rate;
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        msg.innerText = 'Error fetching data';
+    }
+});
